4e7c09e706f53a875fc7541e003e9e0bafb113df
zcq <zcq960921@163.com>
2020-01-01 15:42:21 +0800

    扫描mybatis通用mapper

--- foodie-dev/foodie-dev-api/src/main/java/com/zcq/Application.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/zcq/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+//扫描mybatis通用mapper
+@MapperScan(basePackages = "com.zcq.mapper")
 public class Application {
 
     public static void main(String[] args) {
