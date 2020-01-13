fb0b8fec9d528337e0e234aabe72700cc5404757
zcq960921 <zcq960921@gamil.com>
2020-01-13 21:42:15 +0800

    文件上传

--- foodie-dev/foodie-dev-api/src/main/java/com/zcq/controller/BaseController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/zcq/controller/BaseController.java
@@ -21,5 +21,12 @@ public class BaseController {
     // 微信支付成功 -> 支付中心 -> 天天吃货平台
     //                       |-> 回调通知的url
     String payReturnUrl = "http://api.z.mukewang.com/foodie-dev-api/orders/notifyMerchantOrderPaid";
+    // 用户上传头像的位置
+    public static final String IMAGE_USER_FACE_LOCATION = File.separator + "workspaces" +
+            File.separator + "images" +
+            File.separator + "foodie" +
+            File.separator + "faces";
+//    public static final String IMAGE_USER_FACE_LOCATION = "/workspaces/images/foodie/faces";
+
 
 }
--- foodie-dev/foodie-dev-api/src/main/java/com/zcq/controller/center/CenterUserController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/zcq/controller/center/CenterUserController.java
@@ -38,6 +38,86 @@ public class CenterUserController extends BaseController {
     @Autowired
     private CenterUserService centerUserService;
 
+
+    @ApiOperation(value = "用户头像修改", notes = "用户头像修改", httpMethod = "POST")
+    @PostMapping("uploadFace")
+    public ZCQJSONResult uploadFace(
+            @ApiParam(name = "userId", value = "用户id", required = true)
+            @RequestParam String userId,
+            @ApiParam(name = "file", value = "用户头像", required = true)
+                    MultipartFile file,
+            HttpServletRequest request, HttpServletResponse response) {
+
+        // .sh .php
+
+        // 定义头像保存的地址
+        String fileSpace = IMAGE_USER_FACE_LOCATION;
+        // 在路径上为每一个用户增加一个userid，用于区分不同用户上传
+        String uploadPathPrefix = File.separator + userId;
+
+        // 开始文件上传
+        if (file != null) {
+            FileOutputStream fileOutputStream = null;
+            try {
+                // 获得文件上传的文件名称
+                String fileName = file.getOriginalFilename();
+
+                if (StringUtils.isNotBlank(fileName)) {
+
+                    // 文件重命名  ZCQ-face.png -> ["ZCQ-face", "png"]
+                    String fileNameArr[] = fileName.split("\\.");
+
+                    // 获取文件的后缀名
+                    String suffix = fileNameArr[fileNameArr.length - 1];
+
+                    if (!suffix.equalsIgnoreCase("png") &&
+                            !suffix.equalsIgnoreCase("jpg") &&
+                            !suffix.equalsIgnoreCase("jpeg") ) {
+                        return ZCQJSONResult.errorMsg("图片格式不正确！");
+                    }
+
+                    // face-{userid}.png
+                    // 文件名称重组 覆盖式上传，增量式：额外拼接当前时间
+                    String newFileName = "face-" + userId + "." + suffix;
+
+                    // 上传的头像最终保存的位置
+                    String finalFacePath = fileSpace + uploadPathPrefix + File.separator + newFileName;
+                    // 用于提供给web服务访问的地址
+                    uploadPathPrefix += ("/" + newFileName);
+
+                    File outFile = new File(finalFacePath);
+                    if (outFile.getParentFile() != null) {
+                        // 创建文件夹
+                        outFile.getParentFile().mkdirs();
+                    }
+
+                    // 文件输出保存到目录
+                    fileOutputStream = new FileOutputStream(outFile);
+                    InputStream inputStream = file.getInputStream();
+                    IOUtils.copy(inputStream, fileOutputStream);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (fileOutputStream != null) {
+                        fileOutputStream.flush();
+                        fileOutputStream.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } else {
+            return ZCQJSONResult.errorMsg("文件不能为空！");
+        }
+
+
+        return ZCQJSONResult.ok();
+    }
+
+
     @ApiOperation(value = "修改用户信息", notes = "修改用户信息", httpMethod = "POST")
     @PostMapping("update")
     public ZCQJSONResult update(
